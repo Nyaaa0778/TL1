a5356c452c0ce7146b6ada925abf6c160cb54bfa
nya <k024g0093@g.neec.ac.jp>
2026-06-19 14:06:12 +0900

    playerスポーン位置を指定できるように変更

--- addons/level_editor/__init__.py
+++ addons/level_editor/__init__.py
@@ -1,8 +1,25 @@
+bl_info = {
+    "name": "LevelEditor",
+    "author": "Taro Kamata",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "LevelEditor",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
 import bpy
 import json
 import os
 import socket
 import blf
+
+# 各モジュールをインポート
+from . import export_scene, mesh_tools, properties, ui, collider, disabled, spawn
+
 
 # グローバル状態保持用
 loaded_replay_data = None
@@ -359,12 +376,23 @@ class VIEW3D_PT_game_debugger(bpy.types.Panel):
                 else:
                     box.label(text="Status: No bugs in current frame", icon='CHECKMARK')
 
-classes = (
+local_classes = (
     IMPORT_OT_replay_json,
     CONNECT_OT_game_server,
     DISCONNECT_OT_game_server,
     TAKEOVER_OT_game_state,
     VIEW3D_PT_game_debugger,
+)
+
+# 各モジュール内で定義された classes タプルを展開して結合
+classes = (
+    *export_scene.classes,
+    *mesh_tools.classes,
+    *properties.classes,
+    *ui.classes,
+    *disabled.classes,
+    *spawn.classes,
+    *local_classes,
 )
 
 def register_handlers():
@@ -418,3 +446,53 @@ def unregister_handlers():
         del bpy.types.Scene.socket_port
     if hasattr(bpy.types.Scene, "socket_connected"):
         del bpy.types.Scene.socket_connected
+
+# 3Dビューの「追加 > メッシュ」メニューに追加する項目
+def menu_func(self, context):
+    self.layout.separator()
+    self.layout.operator(spawn.MYADDON_OT_spawn_create_symbol.bl_idname, text="出現ポイントシンボルの作成")
+
+# Add-On有効化時コールバック
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    # タイムライン同期・ビューポート描画等のイベント登録
+    register_handlers()
+
+    # メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(ui.TOPBAR_MT_my_menu.submenu)
+
+    # 「追加 > メッシュ」メニューに項目を追加
+    bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
+
+    # 3Dビューに描画関数を追加
+    collider.DrawCollider.handle = bpy.types.SpaceView3D.draw_handler_add(
+        collider.DrawCollider.draw_collider, (), "WINDOW", "POST_VIEW"
+    )
+    
+    print("レベルエディタが有効化されました。")
+
+# Add-On無効化時コールバック
+def unregister():
+    # タイムライン同期・ビューポート描画等のイベント登録解除
+    unregister_handlers()
+
+    # 「追加 > メッシュ」から削除（エラー防止のため最初に実行）
+    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
+
+    # メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(ui.TOPBAR_MT_my_menu.submenu)
+
+    # 3Dビューの描画関数を削除
+    if collider.DrawCollider.handle:
+        bpy.types.SpaceView3D.draw_handler_remove(collider.DrawCollider.handle, "WINDOW")
+    
+    # 登録した順番と逆に解除していく（エラー防止）
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
+    print("レベルエディタが無効化されました。")
+
+if __name__ == "__main__":
+    register()
